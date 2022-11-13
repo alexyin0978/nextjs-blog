e0d06bd976b17ee69651135880652dfd7cccc0e8
alex <yinchihkai0909@gmail.com>
2022-11-13 12:00:22 +0800

    add: 把教學檔案都push上去

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+
+import Layout from "../../components/layout";
+import Date from '../../components/date';
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+import utilStyles from '../../styles/utils.module.css';
+
+export async function getStaticPaths(){
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false, // any paths not returned by getStaticPaths will result in a 404 page
+  };
+}
+
+export async function getStaticProps({params}){
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({postData}){
+  return (
+    <Layout>
+      <Head>
+        {postData.title}
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
